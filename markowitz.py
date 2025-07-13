4928a20975986d5df31524de128a1313c6e8d0af
scinii <roberto.schinina02@gmail.com>
2025-07-13 19:18:47 +0200

    oop_markowitz

--- markowitz.py
+++ markowitz.py
@@ -3,42 +3,49 @@ import numpy as np
 import pandas as pd
 
 
-def deterministic_markowitz(tickers, r_risk_free, risk_aversion, divers = [0, None] ):
+class MarkowitzPortfolio:
 
-    """
-    :param tickers: a list of tickers
-    :param r_risk_free: return of the risk-free asset
-    :param risk_aversion: coefficient that measures how risk-adverse the user is
-    :param divers: list, its elements give and lower and upper bound (respectively) for the number of stocks
-    :return: weights to assign to the stocks and risk-free asset
-    """
+    def get_returns(self, tickers):
 
-    # some placeholder values to test while we wait for Job's function
+        return np.array([1.2, 1.1, 1.3])
 
-    sigma = np.matrix([[1.5, 0.5, 2], [0.5, 2, 0], [2, 0, 5]])
-    sigma = pd.DataFrame(sigma, index=range( len(tickers) ), columns=range( len(tickers) ))
-    r = np.array([1.2, 1.1, 1.3])
+    def get_covariances(self, tickers):
 
-    portfolio = AMPL()
-    portfolio.read("deterministic_markowitz.mod")
+        sigma = np.matrix([[1.5, 0.5, 2], [0.5, 2, 0], [2, 0, 5]])
+        sigma = pd.DataFrame(sigma, index=range(len(tickers)), columns=range(len(tickers)))
 
-    portfolio.set["S"] = list(range(3))
+        return sigma
 
-    portfolio.param["r_risk_free"] = r_risk_free
-    portfolio.param["r"] = r
-    portfolio.param["Sigma"] = sigma
-    portfolio.param["risk_aversion"] = risk_aversion
-    portfolio.param["lower_divers"] = divers[0]
-    portfolio.param["upper_divers"] = divers[1] if divers[1] is not None else len(tickers)
+    def __init__(self, tickers):
 
-    portfolio.option["solver"] = "highs"
-    portfolio.option["verbose"] = False
-    portfolio.solve()
+        self.returns = self.get_returns(tickers)
+        self.sigma = self.get_covariances(tickers)
+        self.portfolio = AMPL()
+        self.portfolio.option["solver"] = "highs"
+        self.portfolio.option["verbose"] = False
 
-    return portfolio
+    def output_weights(self):
+        self.portfolio.solve()
+        print(self.portfolio.var["x"].to_dict())
+        print(self.portfolio.var["x_risk_free"].value())
 
 
-result = deterministic_markowitz( ["AA", "AAPL", "TSLA"], 1.01, 1, [0,3] )
-print(result.var["x"].to_dict())
-print(result.var["x_risk_free"].value())
+class DeterministicMarkowitz(MarkowitzPortfolio):
 
+    def __init__(self, tickers, r_risk_free, risk_aversion, divers, file_path):
+
+        super().__init__(tickers)
+
+        self.portfolio.read(file_path)
+        self.portfolio.set["S"] = list(range(len(tickers)))
+        self.portfolio.param["r_risk_free"] = r_risk_free
+        self.portfolio.param["r"] = self.returns
+        self.portfolio.param["Sigma"] = self.sigma
+        self.portfolio.param["risk_aversion"] = risk_aversion
+        self.portfolio.param["lower_divers"] = divers[0]
+        self.portfolio.param["upper_divers"] = divers[1] if divers[1] is not None else len(tickers)
+
+
+
+DM = DeterministicMarkowitz(["AA", "AAPL", "TSLA"], 1.01, 1, [1,3], 'deterministic_markowitz.mod')
+DM.output_weights()
